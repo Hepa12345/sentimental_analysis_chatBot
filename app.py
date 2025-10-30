d91f9f3cf41fb282f0825b79b1cbd87aa58ab9ab
Hepa12345 <hemanthborra471@gmail.com>
2025-10-30 23:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from chatbot_model import predict_sentiment
+import os
 
 app = Flask(__name__)
 
@@ -22,6 +23,9 @@ def chat():
     return render_template("index.html", user_input=user_input, sentiment=sentiment, response=response)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
 
 
